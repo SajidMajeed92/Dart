fadff38376caafeb9333691ea1d70c79ddaac233
sajid <sajidmajeed.ist@gmail.com>
2023-04-21 00:54:54 +0500

    varibale declaration in Dart

---
+++ variables.dart
@@ -0,0 +1,6 @@
+main() {
+  // dataType variableName = Initial Value
+  // Dart styling convention to name variables using lower camel case
+  int myfirstDartVariable = 5;
+  print(myfirstDartVariable);
+}
