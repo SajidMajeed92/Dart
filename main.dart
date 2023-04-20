f1d3d61a466ecdcebc6de2ebced55838a3110985
sajid <sajidmajeed.ist@gmail.com>
2023-04-20 23:49:02 +0500

    first Program in Dart

--- main.dart
+++ main.dart
@@ -1,4 +1,10 @@
 main() {
   // Printing the text 'Welcome to learning Dart'
   print("Welcome to learning Dart ");
+  // Explaining above code step by step
+  // print() is used to display the output of the code on the console.
+  // semi-colon (;) is used to end the statement
+  //  Double slash is used to comment the code in Dart
+  //main() function gets executed in order of appearance.
+  print("Hello Worl from Drat");
 }
