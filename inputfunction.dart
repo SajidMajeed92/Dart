7b425a7e6c34b81fd246b3e144e2a9b3be80706e
sajid <sajidmajeed.ist@gmail.com>
2023-04-21 00:38:42 +0500

    Taking Input from user

---
+++ inputfunction.dart
@@ -0,0 +1,9 @@
+// The dart:io library provides I/O (Input/Output) support.
+import 'dart:io';
+
+main() {
+  //readLineSync() method (function) reads a line from the input.
+  // stdin stands for standard input 
+  print("Hello"+ " " + stdin.readLineSync()!);
+
+}
